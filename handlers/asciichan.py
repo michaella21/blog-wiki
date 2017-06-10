4a3b32cb315302254ef9a9419542e21f0f555c9c
Michelle Park <michelle@Hangmis-Air.fios-router.home>
2017-06-09 20:19:08 -0400

    For ASCII page, get a geo loaction from IP address, save it to DB and map it to G-map for the recent entries

--- handlers/asciichan.py
+++ handlers/asciichan.py
@@ -2,6 +2,9 @@ import os
 import webapp2
 import jinja2
 import time
+import urllib2
+import json
+from xml.dom import minidom
 
 from google.appengine.ext import db
 
@@ -20,6 +23,45 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kwargs):
 		self.write(self.render_str(template, **kwargs))
 
+#using api from hostip, which doesn't work any more, so use a different website with json 
+"""IP_url= "http://api.hostip.info/?ip="
+def get_coords(ip):
+	pass
+	url = IP_url +ip
+	content = None
+	try:
+		content = urllib2.urlopen(url).read()
+	except urllib2.URLError: 
+		return
+	if content:
+		#parse the xml and find the coordinate
+		d = minidom.parseString(content)
+		coords_node= d.getElementsByTagName("gml:coordinates")
+		lon, lat = str(coords_node[0].firstChild.data).split(',')
+		return db.GeoPt(lat, lon) """
+
+#look up the location by ip address		
+IP_url = "http://ip-api.com/json/"
+def get_coords(ip):
+	pass
+	url = IP_url + ip
+	content = None
+	try:
+		content = urllib2.urlopen(url).read()
+	except urllib2.URLError:
+		return
+	if content:
+		d =  json.loads(content)
+		lat, lon = d['lat'], d['lon']
+		return db.GeoPt(lat,lon)
+
+#from the ip address given get_coords(ip), map it to google maps
+GMAPS_url = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_img(points):
+	markers = '&'.join('markers=%s,%s' %(point.lat, point.lon) for point in points)
+	return GMAPS_url + markers
+
+
 #Create entities (store art entered in Datastore)
 class Art(db.Model):
 	title = db.StringProperty(required = True)
@@ -27,13 +69,29 @@ class Art(db.Model):
 	#to prevent you from adding bad data into your db. 
 	art = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)#automatically adding the current time
-
+	coords = db.GeoPtProperty()
 
 class Asciichan(Handler):
 	def render_front(self, title="", art="", error=""):
-		arts = db.GqlQuery("SELECT * FROM Art Order by created DESC")
+		arts = db.GqlQuery("SELECT * FROM Art Order by created DESC LIMIT 10")
 
-		self.render("ascii_front.html", title = title, art = art, error = error, arts = arts)
+		#need to iterate over arts and we do not want to run multiple queries. 
+		arts = list(arts)
+		
+
+		#find which arts have coords
+		#can use grnerator: filter(None) means return everything but none			
+		points = filter (None, (a.coords for a in arts))
+		#self.write(repr(points)) to check whether it works properly
+
+		#if we have any arts with coords, make an image url, display the img url
+		img_url = None
+		if points:
+			img_url = gmaps_img(points)
+
+		self.render("ascii_front.html", title = title, art = art, error = error, arts = arts, img_url = img_url)
+
+	
 
 
 	def get(self):
@@ -45,6 +103,12 @@ class Asciichan(Handler):
 
 		if title and art:
 			a = Art(title = title, art = art )
+			#lookup the user's coordinates from their IP
+			coords = get_coords(self.request.remote_addr)#request ip address
+			#if we have coordinates, add them to art.
+			if coords:
+				a.coords = coords
+			 
 			a.put()
 			time.sleep(1.0)
 
